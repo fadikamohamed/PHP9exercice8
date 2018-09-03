508b48b09adaba81078725e77fde2762a5a02a95
fadikamohamed <fadika.mohamed02@gmail.com>
2018-09-03 15:27:40 +0200

    exercice terminé

---
+++ fonction.php
@@ -0,0 +1,17 @@
+<?php
+function biloute($param1 = 'dsfsdfhsjf', $param2 = 'hsdbvhvbfk') {
+  echo 'Bonjour ' . $param1 . $param1 . ' ' ;
+}
+
+ ?>
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8" />
+    <title></title>
+  </head>
+  <body>
+    <?php biloute('', 'djhsdfld'); biloute();?>
+
+  </body>
+</html>
---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.css" />
+    <title>Exercice 8</title>
+  </head>
+  <body>
+      <div class="container">
+          <div class="row justify-content-center bg-dark text-white">
+              <div class="col-md-12">
+                  <p>
+                      <?= date('j-m-Y', strtotime('-22 days')) ?>
+                  </p>
+              </div>
+          </div>
+      </div>
+  </body>
+</html>
